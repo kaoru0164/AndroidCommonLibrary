c7f6410256c241a1dcae3c0bf48c3584061df627
kaoru0164 <kaoru0164@tf6.so-net.ne.jp>
2017-11-26 17:22:49 +0900

    Create project files.

---
+++ CommonLibrary/app/src/main/java/jp/k_toybox/android/library/common/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.k_toybox.android.library.common
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
